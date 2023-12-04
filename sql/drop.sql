25c080396c17e03c5c42d49ece51fdfebec65c1e
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2023-12-04 23:35:48 +0100

    Script for droping database with connection closing

--- sql/drop.sql
+++ sql/drop.sql
@@ -1,5 +1,6 @@
+-- !!! In Microsoft SQL Server Management Studio you have to close old script which used this database and create new !!!
+
 -- Close all connections to the database
--- alter database KomoraCelna set single_user with rollback immediate
 ALTER DATABASE KomoraCelna SET SINGLE_USER WITH ROLLBACK IMMEDIATE
 
 -- Deleting whole database
