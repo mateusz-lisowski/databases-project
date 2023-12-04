17a5c87646669f53590facfc77981db56165551e
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2023-12-04 23:40:51 +0100

    Working database creation

--- sql/create_database.sql
+++ sql/create_database.sql
@@ -3,18 +3,3 @@ CREATE DATABASE KomoraCelna;
 
 -- Set our new database to be used
 USE KomoraCelna ;
-
--- Function to check if given date is grater than other date
-CREATE FUNCTION CheckDateGreaterThanInControl (
-    @dateToCheck DATE
-)
-RETURNS BIT
-AS
-BEGIN
-    DECLARE @result BIT
-    IF EXISTS (SELECT 1 FROM Kontrole WHERE Data_kontroli <= @dateToCheck)
-        SET @result = 0
-    ELSE
-        SET @result = 1
-    RETURN @result
-END;
