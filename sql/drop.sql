cb67e7cf85aab43ea223c296766aa011090429d3
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2023-12-04 23:35:30 +0100

    Script for droping database with connection closing

--- sql/drop.sql
+++ sql/drop.sql
@@ -1,2 +1,6 @@
+-- Close all connections to the database
+-- alter database KomoraCelna set single_user with rollback immediate
+ALTER DATABASE KomoraCelna SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+
 -- Deleting whole database
 DROP DATABASE KomoraCelna;
