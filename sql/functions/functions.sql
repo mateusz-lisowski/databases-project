536be4cdb946901631294960adac22760fea284d
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2023-12-05 21:43:41 +0100

    Refactored functions scripts to new directory

---
+++ sql/functions/functions.sql
@@ -0,0 +1,16 @@
+-- !!! Run this script before creating tables !!!
+
+-- Function to check if given date is grater than other date
+CREATE FUNCTION CheckDateGreaterThanInControl (
+    @dateToCheck DATE
+)
+RETURNS BIT
+AS
+BEGIN
+    DECLARE @result BIT
+    IF EXISTS (SELECT 1 FROM Kontrole WHERE Data_kontroli <= @dateToCheck)
+        SET @result = 0
+    ELSE
+        SET @result = 1
+    RETURN @result
+END;
