0534514ad504c33f94eed295a76d8f9012aa3243
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2023-12-07 18:23:15 +0100

    Working upadate demo

--- sql/update.sql
+++ sql/update.sql
@@ -1,13 +1,17 @@
+-- Show state before making changes
+SELECT * FROM Kategorie_towarow ORDER BY Nazwa_kategorii
+SELECT * FROM Stanowiska ORDER BY Nazwa_stanowiska
+
 -- Update industrial category
 UPDATE Kategorie_towarow
-SET Nazwa_kategorii = 'Przemysłowe i gospodarstwa domowego', Opis_kategorii = 'Artykuły używane w przemyśle i gospodarstwie domowym.'
+SET Nazwa_kategorii = 'Przemysłowe i chemia domowa', Opis_kategorii = 'Artykuły używane w przemyśle i chemia gospodarstwie domowym.'
 WHERE Nazwa_kategorii = 'Przemysłowe';
 
 
 -- Update AGD category 
 UPDATE Kategorie_towarow
-SET Nazwa_kategorii = 'AGD', Opis_kategorii = 'Artykuły używane w gospodarstwie domowym.'
-WHERE Nazwa_kategorii = 'Przemysłowe i gospodarstwa domowego';
+SET Nazwa_kategorii = 'Artykuły gospodarstwa domowego', Opis_kategorii = 'Artykuły używane w gospodarstwie domowym.'
+WHERE Nazwa_kategorii = 'AGD';
 
 
 -- Update inspector positions 
@@ -20,3 +24,8 @@ WHERE Nazwa_stanowiska = 'Inspektor celny';
 UPDATE Stanowiska
 SET Ilosc_godzin_pracy = 8
 WHERE Nazwa_stanowiska = 'Doradca celny';
+
+
+-- Show state after making changes
+SELECT * FROM Kategorie_towarow ORDER BY Nazwa_kategorii
+SELECT * FROM Stanowiska ORDER BY Nazwa_stanowiska
