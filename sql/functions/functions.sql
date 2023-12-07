9d39b47cef7e2fce3aed152048a6378132824a94
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2023-12-07 18:19:40 +0100

    Extended function to check package id

--- sql/functions/functions.sql
+++ sql/functions/functions.sql
@@ -2,13 +2,14 @@
 
 -- Function to check if given date is grater than other date
 CREATE FUNCTION CheckDateGreaterThanInControl (
+    @packageNumber UNIQUEIDENTIFIER,
     @dateToCheck DATE
 )
 RETURNS BIT
 AS
 BEGIN
     DECLARE @result BIT
-    IF EXISTS (SELECT 1 FROM Kontrole WHERE Data_kontroli <= @dateToCheck)
+    IF EXISTS (SELECT 1 FROM Kontrole WHERE Data_kontroli > @dateToCheck AND Numer_przesylki = @packageNumber)
         SET @result = 0
     ELSE
         SET @result = 1
