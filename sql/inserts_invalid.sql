cd371b616be629e1874e00e38607537363b28d4f
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2023-12-08 01:24:53 +0100

    Reworked constrains testing to work proprerly

--- sql/inserts_invalid.sql
+++ sql/inserts_invalid.sql
@@ -1,3 +1,6 @@
+------------ Insert valid records to the tables ------------
+
+
 -- Insert documents with false status check
 INSERT INTO Dokumenty_celne (Numer_deklaracji, ID_klienta, Nazwa_oplaty, Kwota_cla, Status_deklaracji) 
 VALUES 
@@ -8,7 +11,6 @@ VALUES
 -- Insert values which will create error to packages table
 INSERT INTO Przesylki (Numer_przesylki, ID_towaru, Numer_deklaracji, Waga)
 VALUES
-    ('fdf35601-c99f-4017-8a27-a506e51f002b', '43ee2287-1ac6-4bda-b43c-9e215218f0e3', 'e10f9128-a9ac-4ce3-b920-6cec16a09fb6', 6900),
     ('f5a737d2-3975-4c6f-b97a-66bd5f934878', '43ee2287-1ac6-4bda-b43c-9e215218f0e3', '6b19020b-02cf-4ca8-8e4c-295530a0828c', 69010);
 
 
@@ -18,7 +20,17 @@ VALUES
     ('70050556789', 'f5a737d2-3975-4c6f-b97a-66bd5f934878', 0, 'Kontrola wykazała braki');
 
 
+------------ Try to insert invalid records to the tables ------------
+
+
+-- Insert values which will create error to packages table
+INSERT INTO Przesylki (Numer_przesylki, ID_towaru, Numer_deklaracji, Waga)
+VALUES
+    ('fdf35601-c99f-4017-8a27-a506e51f002b', '43ee2287-1ac6-4bda-b43c-9e215218f0e3', 'e10f9128-a9ac-4ce3-b920-6cec16a09fb6', 6900);
+
+
 -- Insert values which will create error to borders crossings table
 INSERT INTO Przejscia_graniczne (Numer_przesylki, ID_miejsca, PESEL) 
 VALUES
     ('f5a737d2-3975-4c6f-b97a-66bd5f934878', '169d646c-5dcb-4aa9-b970-3c00412cb0f3', '95060667890');
+    
