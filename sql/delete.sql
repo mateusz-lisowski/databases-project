ec631c53c49bd11f77471032869f80f7bc7ff825
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2023-12-08 01:22:49 +0100

    Fixed delete to better show cascade

--- sql/delete.sql
+++ sql/delete.sql
@@ -1,23 +1,34 @@
+------------ Delete records from categories table ------------
+
 -- Show state before making changes
-SELECT * FROM Kategorie_towarow ORDER BY Nazwa_kategorii
-SELECT * FROM Stanowiska ORDER BY Nazwa_stanowiska
+SELECT * FROM Kategorie_towarow ORDER BY Nazwa_kategorii;
 
 
 -- Deleting empty category
 DELETE FROM Kategorie_towarow
 WHERE Nazwa_kategorii = 'Pusta';
 
- 
+
+-- Show state after making changes
+SELECT * FROM Kategorie_towarow ORDER BY Nazwa_kategorii;
+
+
+------------ Delete records from officers table ------------
+
+
+-- Show state before making changes
+SELECT * FROM Celnicy ORDER BY Nazwa_stanowiska;
+
+
 -- Deleting inspector position
 DELETE FROM Celnicy
 WHERE Nazwa_stanowiska = 'Inspektor celny';
 
 
--- Deleting inspector position
+-- Deleting advisor position
 DELETE FROM Celnicy
-WHERE Nazwa_stanowiska = 'Doradca celny';
+WHERE Nazwa_stanowiska = 'Doradca celny starszy';
 
 
 -- Show state after making changes
-SELECT * FROM Kategorie_towarow ORDER BY Nazwa_kategorii
-SELECT * FROM Stanowiska ORDER BY Nazwa_stanowiska
+SELECT * FROM Celnicy ORDER BY Nazwa_stanowiska;
