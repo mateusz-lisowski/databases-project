258622d4332010c57289802ceec2dd2dcc5b5e9d
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-01-09 22:39:29 +0100

    Queries propositions for first friend

---
+++ sql/queries/queries.sql
@@ -0,0 +1,27 @@
+--  Ile towarów z danej kategori przeszło przez granice w ciągu ostatniego roku. 
+
+SELECT T.Nazwa_kategorii, COUNT(*)
+FROM Towary T
+INNER JOIN Przesylki P ON T.ID_towaru = P.ID_towaru
+INNER JOIN Przejscia_graniczne PG ON P.Numer_przesylki = PG.Numer_przesylki
+WHERE PG.Data_przejscia >= DATEADD(YEAR, -1, GETDATE())
+GROUP BY T.Nazwa_kategorii;
+
+
+-- Ile kontroli przeprowadził każdy z celników
+
+SELECT C.PESEL, C.Imie_celnika, C.Nazwisko_celnika, COUNT(K.ID_kontroli) AS Ilosc_kontroli
+FROM Celnicy C
+LEFT JOIN Kontrole K ON C.PESEL = K.PESEL
+GROUP BY C.PESEL, C.Imie_celnika, C.Nazwisko_celnika
+ORDER BY Ilosc_kontroli DESC
+
+
+-- Ile przesyłek przeszło przez każde z przejść granicznych
+
+SELECT MP.ID_miejsca, MP.Nazwa_miejsca, 
+    (SELECT COUNT(PG.Numer_przesylki) 
+    FROM Przejscia_graniczne PG 
+    WHERE PG.ID_miejsca = MP.ID_miejsca) AS Ilosc_przesylek
+FROM Miesca_przejsc MP
+ORDER BY Ilosc_przesylek DESC
